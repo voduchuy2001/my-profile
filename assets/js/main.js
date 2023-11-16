3597e3ecd2abdf4a38814cab2d51e06bc9ae7551
voduchuy2001 <voduchuy2001@gmail.com>
2023-11-16 14:56:35 +0700

    chores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,107 @@
-// Show menu 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
+  });
 }
-showMenu('nav-toggle','nav-menu')
+window.addEventListener("scroll", scrollActive);
 
-// Remove menu mobile
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+window.addEventListener("scroll", scrollTop);
 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
-// Scroll section
-const sections = document.querySelectorAll('section[id]')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-// Show scroll to top 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollTop)
-
-// Light/Dark Mode 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
-
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
-// Reduce size and print on A4 sheet
-function scaleCv(){
-    document.body.classList.add('scale-cv')
-  }
-  
-  //Remove the size when cv is downloaded
-  
-  function removeScale(){
-    document.body.classList.remove('scale-cv')
-  }
-  
-  //Generate PDF
-  let areaCv = document.getElementById('area-cv')
-  
-  //PDF Generated area
-  let resumeButton = document.getElementById('resume-button')
-  
-  //html2pdf options
-  let opt = {
-    margin: 0,
-    filename: 'VDHuy_Profile.pdf',
-    image: {type: 'jpeg', quality: 0.98},
-    html2canas: {scale: 4},
-    jsPDF: { format: 'a4', orientation: 'portrait'}
-  }
-  //Function to call areaCv and HTML2PDF
-  function generateResume(){
-    html2pdf(areaCv, opt)
-  }
-  
-  
-  resumeButton.addEventListener('click', () =>{
-  // 1. The class .scale-cv is added to body, where it reduces the size
-    scaleCv()
-  
-    // 2. PDF is Generated
-    generateResume()
-  
-    // 3. Remove .scale-cv class after 3s
-    setTimeout(removeScale, 3000)
-  })
+function scaleCv() {
+  document.body.classList.add("scale-cv");
+}
+
+function removeScale() {
+  document.body.classList.remove("scale-cv");
+}
+
+let areaCv = document.getElementById("area-cv");
+
+let resumeButton = document.getElementById("resume-button");
+
+let opt = {
+  margin: 0,
+  filename: "VDHuy_Profile.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
+
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener("click", () => {
+  scaleCv();
+  generateResume();
+  setTimeout(removeScale, 3000);
+});
